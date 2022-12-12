a2768a399923c1131d9e5b699baea1ec299295a9
Tim La <tlamarketing111@gmail.com>
2022-12-12 06:54:23 -0500

    components created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-// import { Sidebar, Navbar } from './components';
-// import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
+import { Sidebar, Navbar } from './components';
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
 const App = () => {
   return (
@@ -12,14 +12,14 @@ const App = () => {
       </div>
 
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        {/* <Navbar /> */}
+        <Navbar />
 
-        {/* <Routes>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-campaign" element={<CreateCampaign />} />
           <Route path="/campaign-details/:id" element={<CampaignDetails />} />
-        </Routes> */}
+        </Routes>
       </div>
       App
     </div>
