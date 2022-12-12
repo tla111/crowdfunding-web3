71d2331535c389f8c9305c2860f71ced994c7886
Tim La <tlamarketing111@gmail.com>
2022-12-12 06:07:22 -0500

    functions set up

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -17,4 +17,12 @@ contract CrowdFunding {
     mapping(uint256 => Campaign) public campaigns;
 
     uint256 public numberOfCampaigns = 0;
+
+    function createCampaign () {}
+
+    function donateToCampaign () {}
+
+    function getDonators() {}
+
+    function getCampaigns() {}
 }
