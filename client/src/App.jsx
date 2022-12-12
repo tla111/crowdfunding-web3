c601749d552ef6b506d3483edbc4c83fdb519a0f
Tim La <tlamarketing111@gmail.com>
2022-12-12 06:56:55 -0500

    imported components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-// import { Sidebar, Navbar } from './components';
+import { Sidebar, Navbar } from './components';
 import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
 const App = () => {
