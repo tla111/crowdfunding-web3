4895e39b9fc6f3d93278676ff2d424729ac13f4e
Tim La <tlamarketing111@gmail.com>
2022-12-13 15:32:54 -0500

    setting up context file

---
+++ client/src/context/index.js
@@ -0,0 +1,13 @@
+import React, { useContext, createContext } from 'react';
+
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+
+}
+
+export const useStateContext = () => useContext(StateContext);
