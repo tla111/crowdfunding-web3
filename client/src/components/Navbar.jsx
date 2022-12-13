57d230b9c2a99ff5a83a97fe27c5cca2a85859dc
Tim La <tlamarketing111@gmail.com>
2022-12-13 12:02:22 -0500

    added props for CustomButton

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,6 +12,8 @@ const Navbar = () => {
   const [toggleDrawer, setToggleDrawer] = useState(false);
   // const { connect, address } = useStateContext();
 
+  const address = "0xab"
+
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
@@ -19,6 +21,18 @@ const Navbar = () => {
         <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
           <img src={search} alt="search" className="w-[15px] h-[15px] object-contain" />
         </div>
+      </div>
+
+      <div className="sm:flex hidden flex-row justify-end gap-4">
+        <CustomButton
+          btnType="button"
+          title={address ? 'Create a campaign' : 'Connect'}
+          styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
+          handleClick={() => {
+            if (address) navigate('create-campaign')
+            else connect()
+          }}
+        />
       </div>
     </div>
   )
