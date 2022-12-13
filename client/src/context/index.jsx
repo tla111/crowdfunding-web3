af92cc15e53e8a03a683def2d73fa6f33759abb9
Tim La <tlamarketing111@gmail.com>
2022-12-13 15:48:42 -0500

    created logic to getCampaigns

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -30,6 +30,23 @@ export const StateContextProvider = ({ children }) => {
         }
     }
 
+    const getCampaigns = async () => {
+        const campaigns = await contract.call('getCampaigns');
+
+        const parsedCampaings = campaigns.map((campaign, i) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+            image: campaign.image,
+            pId: i
+        }));
+
+        return parsedCampaings;
+    }
+
     return (
         <StateContext.Provider
             value={{
@@ -37,7 +54,7 @@ export const StateContextProvider = ({ children }) => {
                 contract,
                 connect,
                 createCampaign: publishCampaign,
-                // getCampaigns,
+                getCampaigns,
                 // getUserCampaigns,
                 // donate,
                 // getDonations
