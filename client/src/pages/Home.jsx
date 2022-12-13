6669c009ac2137379fafde50c5ed6554bd4312c6
Tim La <tlamarketing111@gmail.com>
2022-12-13 15:49:55 -0500

    home page completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-// import { DisplayCampaigns } from '../components';
+import { DisplayCampaigns } from '../components';
 import { useStateContext } from '../context'
 
 const Home = () => {
@@ -9,8 +9,23 @@ const Home = () => {
 
   const { address, contract, getCampaigns } = useStateContext();
 
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
   return (
-    <div>Home</div>
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
   )
 }
 
