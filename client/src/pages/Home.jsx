51a2bb48f6b729d0929abfc06407ea4490288389
Tim La <tlamarketing111@gmail.com>
2022-12-13 15:47:59 -0500

    imported imports & set states for Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+// import { DisplayCampaigns } from '../components';
+import { useStateContext } from '../context'
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getCampaigns } = useStateContext();
+
   return (
     <div>Home</div>
   )
