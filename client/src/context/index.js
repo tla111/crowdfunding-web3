b9c24f2eb74d5459bd8b3515320b933ffc191e74
Tim La <tlamarketing111@gmail.com>
2022-12-13 15:35:01 -0500

    have the tools to connect to wallet

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -8,6 +8,10 @@ const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
     const { contract } = useContract('0xdb727aE4aE51ceC4311a2DD39b7104D08245c5D5');
+    const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+
+    const address = useAddress();
+    const connect = useMetamask();
 }
 
 export const useStateContext = () => useContext(StateContext);
