0801a7dcf46604707c9eb8c7858b76231671b0fb
Tim La <tlamarketing111@gmail.com>
2022-12-13 15:35:42 -0500

    logic for publicCampaign built

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -12,6 +12,23 @@ export const StateContextProvider = ({ children }) => {
 
     const address = useAddress();
     const connect = useMetamask();
+
+    const publishCampaign = async (form) => {
+        try {
+            const data = await createCampaign([
+                address, // owner
+                form.title, // title
+                form.description, // description
+                form.target,
+                new Date(form.deadline).getTime(), // deadline,
+                form.image
+            ])
+
+            console.log("contract call success", data)
+        } catch (error) {
+            console.log("contract call failure", error)
+        }
+    }
 }
 
 export const useStateContext = () => useContext(StateContext);
