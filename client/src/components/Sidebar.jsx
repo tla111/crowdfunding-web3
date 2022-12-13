464c7cf05671f4284804c841dd3739079be20b32
Tim La <tlamarketing111@gmail.com>
2022-12-13 11:52:25 -0500

    building layout for sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,8 +5,16 @@ import { logo, sun } from '../assets';
 import { navlinks } from '../constants';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState('dashboard');
+  
   return (
-    <div>Sidebar</div>
+    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+    <Link to="/">
+      <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+    </Link>
+
+    </div>
   )
 }
 
